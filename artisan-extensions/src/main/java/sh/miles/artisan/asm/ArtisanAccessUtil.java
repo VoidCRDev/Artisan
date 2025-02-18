c6f048ad180dba3eda317643afb0b125a807c413
Y2Kwastaken <mwholder@proton.me>
2025-02-18 17:07:33 -0600

    Cleanup messy impl

--- artisan-extensions/src/main/java/sh/miles/artisan/asm/ArtisanAccessUtil.java
+++ artisan-extensions/src/main/java/sh/miles/artisan/asm/ArtisanAccessUtil.java
@@ -1,5 +1,7 @@
 package sh.miles.artisan.asm;
 
+import sh.miles.artisan.util.ArtisanUtils;
+
 import java.util.Locale;
 import java.util.Map;
 
@@ -14,10 +16,16 @@ import static org.objectweb.asm.Opcodes.ACC_TRANSITIVE;
 
 /**
  * Provides a simple utility to convert from an "access" to the opcode variant
+ *
+ * @since 1.0.0
  */
 public final class ArtisanAccessUtil {
 
     private static final Map<String, Integer> TRANSLATOR = Map.of("public", ACC_PUBLIC, "private", ACC_PRIVATE, "protected", ACC_PROTECTED, "static", ACC_STATIC, "final", ACC_FINAL, "synchronized", ACC_SYNCHRONIZED, "transitive", ACC_TRANSITIVE, "transient", ACC_TRANSIENT);
+
+    private ArtisanAccessUtil() {
+        ArtisanUtils.utilityClass(getClass());
+    }
 
     /**
      * Translates the provided scope into an ASM opcode
@@ -26,6 +34,7 @@ public final class ArtisanAccessUtil {
      *
      * @param scope the scope to translate, will return -1 for null input
      * @return the opcode, or -1
+     * @since 1.0.0
      */
     public static int scopeToOpcode(String scope) {
         if (scope == null) return -1;
